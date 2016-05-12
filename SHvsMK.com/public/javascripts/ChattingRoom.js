420c8199b3605b2922610c91a8ba27c1dfeaecf0
SHvsMK <SHvsMK@gmail.com>
2016-05-12 15:59:15 -0500

    Chatting Room V1 finished

--- SHvsMK.com/public/javascripts/ChattingRoom.js
+++ SHvsMK.com/public/javascripts/ChattingRoom.js
@@ -22,7 +22,6 @@ $(document).ready(function() {
       alert(err);
     },
     success: function(res) {
-      alert(res.message);
       if (res.success == false) {
         window.location.href = '/Signin';
       } else {
@@ -34,28 +33,17 @@ $(document).ready(function() {
 
   var socket = new WebSocket("ws://10.105.76.129:8080?username=" + user);
   socket.onopen = function() {
-    // ack = JSON.stringify({'sender':});
-    // socket.send()
-    alert("yes");
-    alert(socket.readyState);
   }
 
   socket.onmessage = function(msg) {
-    // display = 'div';
     m = JSON.parse(msg.data);
     sender = m.Sender;
     content = m.Message;
     if (sender == user) {
-      alert("same");
+      $('.chat-thread').append('<li class="my_message">' + content + '</li>');
     } else {
-      alert("no");
+      $('.chat-thread').append('<li class="friends_message">' + content + '</li>');
     }
-    // alert(msg.sender);
-    // alert(msg.isTrusted);
-    // alert(msg.data);
-    // alert(JSON.parse(msg));
-    // alert(msg.textMessage);
-    // $(".Message-receive").html('<h1>'+msg.data+'</h1>');
   }
 
   $('.send').click(function() {
@@ -63,21 +51,6 @@ $(document).ready(function() {
     if (message == "")
       return;
     data = JSON.stringify({'sender': user, 'message': message});
-    alert(data);
     socket.send(data);
   });
-  // $('.try').click(function() {
-  //   $.ajax({
-  //       url: "http://localhost:8080/",
-  //       method: "GET",
-  //       success: function(data){
-  //         var key = Object.keys(data);
-  //         alert(key);
-  //         alert(data['hello']);
-  //       },
-  //       error: function(err){
-  //         alert("no");
-  //       }
-  //     });
-  // });
 });
